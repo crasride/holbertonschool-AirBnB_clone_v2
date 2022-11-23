50fe9b66d443f4e620a4db7c4902873f74dd0c13
Hedgebzh <axel.gore35@gmail.com>
2022-11-23 11:38:21 +0100

    dbstorage and update

---
+++ models/engine/db_storage.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Defines the DBStorage engine."""
+from models.base_model import Base
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from sqlalchemy import create_engine
+
+class DBStorage:
+    __engine = None
+    __session = None
+
+    def all(self, cls=None):
+
+    def new(self, obj):
+        """Add obj to the current database session."""
+        self.__session.add(obj)
+
+    def save(self):
+        """Commit all changes to the current database session."""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """Delete obj from the current database session."""
+        if obj is not None:
+            self.__session.delete(obj)
+
