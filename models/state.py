f75af504e03ed7f3d4284b61e3dc61b0394267ce
Hedgebzh <axel.gore35@gmail.com>
2022-11-24 22:16:56 +0100

    sqlalchemy adds for city, place, review, state and user

--- models/state.py
+++ models/state.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models.city import City
 import models
 from os import getenv
+from models.base_model import Base
+from models.base_model import BaseModel
+from models.city import City
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     # Task 6
     __tablename__ = 'states'
     name = Column(String(128), nullable=True)
+    cities = relationship("City",  backref="state", cascade="delete")
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         cities = relationship('City', backref='state', cascade='all, delete')
