28e411aa84c0e473b72226bcd26148e37b6b8207
crasride <jose.dfernandez@yahoo.fr>
2022-11-25 17:35:23 +0100

    update place origin

--- models/user.py
+++ models/user.py
@@ -4,7 +4,6 @@ from models.base_model import Base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column
 from sqlalchemy import String
-from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -14,5 +13,3 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
