34a7f4ff95860ff0ed8c0dee046e6960c73d6178
crasride <jose.dfernandez@yahoo.fr>
2022-11-29 12:04:37 +0100

    8. List of states fix pycode

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,15 +11,15 @@ from flask import render_template
 app = Flask(__name__)
 
 
+# you must remove the current SQLAlchemy Session
 @app.teardown_appcontext
 def close_session(exception):
-# you must remove the current SQLAlchemy Session
     storage.close()
 
 
+# display a HTML page: list of states
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-# display a HTML page: list of states
     states = storage.all("State")
     return render_template("7-states_list.html", states=states)
 
